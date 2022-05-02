5f1eb71814398b471faddc54559c343159f8fcf8
Josue <josuecarlosos2@gmail.com>
2022-05-02 10:16:02 -0300

    listener

---
+++ lib/6-listeners/listen.dart
@@ -0,0 +1,17 @@
+void main() async {
+  print("Início do programa!");
+
+  var intervalo = Duration(seconds: 2);
+  var stream = Stream<String>.periodic(intervalo, (v) => "Número: ${v}");
+
+  stream = stream.take(10);
+
+  //com o await for o programa ficava parado, mas agora usaremos uma forma de escuta da stream
+  //evitando o congestionamento do sistema
+  stream.listen((valor) {
+    print(valor);
+  });
+
+  print(
+      "Executou o programa de ponta a ponta sem fazer o congestionamento por causa do Stream");
+}
