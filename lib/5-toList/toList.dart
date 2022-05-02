70e5c3cadfd1c99697af0b7f2bad0ee805ceb225
Josue <josuecarlosos2@gmail.com>
2022-05-02 10:07:33 -0300

    toList

---
+++ lib/5-toList/toList.dart
@@ -0,0 +1,13 @@
+void main() async {
+  var intervalo = Duration(seconds: 2);
+  var stream = Stream<String>.periodic(intervalo, (v) => "Número: ${v}");
+
+  stream = stream.take(5);
+
+  // vamos fazer com que o stream pareça ser uma forma "síncrona" fazendo com que o
+  // retorno seja de uma vez só. vamos resgatar todos os valores dela de apenas uma vez
+
+  final data = await stream.toList();
+
+  print(data);
+}
