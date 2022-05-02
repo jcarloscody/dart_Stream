d426457c995d4d47eb1844bc8dcd30d84b0ba787
Josue <josuecarlosos2@gmail.com>
2022-05-02 09:46:45 -0300

    take

---
+++ lib/3-take/take.dart
@@ -0,0 +1,11 @@
+void main() async {
+  var intervalo = Duration(seconds: 2);
+  var stream = Stream<String>.periodic(intervalo, (v) => "Número: ${v}");
+
+  //ele vai pegar apenas 5 requisições. com ela vc pode fatiar a stream
+  stream = stream.take(5);
+
+  await for (var item in stream) {
+    print(item);
+  }
+}
---
+++ lib/3-take/takeWhile.dart
@@ -0,0 +1,13 @@
+void main() async {
+  var intervalo = Duration(seconds: 2);
+  var stream = Stream<int>.periodic(intervalo, (v) => v);
+
+  //ele recebe uma condição
+  stream = stream.takeWhile((element) => element < 2);
+
+  await for (var item in stream) {
+    print(item);
+  }
+
+  print("FIM");
+}
